5319f1f522a9e2bb3ef55d322d0b64089e5e3d0d
Camilo Andres Salinas Martinez <camilo@ggitapps.com>
2018-04-09 09:26:40 -0500

    Cambios (Cliente)

--- src/view/TaxiTripsManagerView.java
+++ src/view/TaxiTripsManagerView.java
@@ -114,116 +114,246 @@ public class TaxiTripsManagerView
 				break;
 
 				//1A	
-			case 2:
+case 2: //1A
 				
-				System.out.println("Ingrese el Id de la zona");
-				String zona1A=sc.next();
-				
-				int area= Integer.parseInt(zona1A);
-				
-				LinkedSimpleList<Servicio> servicesReq1= Controller.darServiciosEnAreaOrdenCronologico(area);
-					
-				
-				Servicio actual= null;
-				if(servicesReq1!=null){
-					System.out.println("Se encontraron "+servicesReq1.size()+" servicios.");
-					
-					for(int i=0; i<servicesReq1.size();i++){
-						actual= servicesReq1.get(i);
-						if(actual!=null){
-							System.out.println("--------------------------------------------------------------------------------------------");
-							System.out.println("Id Trip : "+ actual.getTripId());
-							System.out.println("Seconds : "+ actual.getTripSeconds());
-							try {
-								System.out.println("Date Start : "+ actual.getStartTime().toString());
-							} catch (ParseException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-							System.out.println("--------------------------------------------------------------------------------------------");
-						
-							
-						}
-					}
-				if(servicesReq1.size()==0){
-					System.out.println("--------------------------------------------------------------------------------------------");
-					System.out.println("No hay servicios en esta area");
-					System.out.println("--------------------------------------------------------------------------------------------");
-				
-					}
+				//id zona
+				System.out.println("Ingrese el id de la zona");
+				String zonaIDReq1A = sc.next();
+				int idZona = 0;
+				try {
+					idZona = Integer.parseInt(zonaIDReq1A);
 				}
-				else{
-					System.out.println("--------------------------------------------------------------------------------------------");
-					System.out.println("No hay servicios en esta area");
-					System.out.println("--------------------------------------------------------------------------------------------");
-				
+				catch (Exception e) {
+					System.err.println("id invalido");
+					break;
 				}
+
+				//nombre compania
+				System.out.println("Ingrese el nombre de la compania");
+				sc.nextLine();
+				String companiaReq1A = sc.nextLine();
 				
+				//Req 1A
+				// Mostrar el Id del (de los) taxi(s) y fecha/hora de inicio de sus servicios iniciando en la zona dada
+				IList<TaxiConServicios> listaTaxis = Controller.R1A(idZona, companiaReq1A);
+				for(TaxiConServicios taxi: listaTaxis){
+					taxi.print();
+				}
+
 				break;
-				
+
 			case 3: //2A
-				System.out.println("Ingrese la distancia de consulta.");
-				String distanciaAD=sc.next();
-				
-				LinkedSimpleList<Servicio> req1B=Controller.darServiciosPorDistanciaRecorridaMillas(distanciaAD);
-				//TODO
-				//Mostrar la informacion de acuerdo al enunciado
-				
-				//Calcula Rango conulta
-				
-				double dis=Double.parseDouble(distanciaAD);
-				
-				int contadorDis=1;
-				
-				while(dis>=1.0){
-					dis=dis-1;
-					contadorDis++;
-					if(dis<=0){
-						contadorDis--;
-					}
+
+				//Duracion de la consulta
+				System.out.println("Ingrese la duracion de consulta (segundos)");
+				String duracionS = sc.next();
+				int duracion = 0;
+				try {
+					duracion = Integer.parseInt(duracionS);
+				} catch (Exception e) {
+					System.err.println("Duracion invalida");
+					break;
 				}
-				//Imprime respuesta metodo.
-				if(req1B!=null){
-				System.out.println("--------------------------------------------------------------------------------------------");
-				System.out.println("Conjunto de Serv. distancia (millas) : Mayores a "+  (contadorDis-1) + " y menores a " + contadorDis );
-				System.out.println("Numero de servicios : "+ req1B.size());
-				
-				req1B.listing();
-				Servicio servi= req1B.getCurrent();
-				
-				
-				for(int j=0; j<req1B.size();j++){
-					
-					if(servi!=null){
-						
-					System.out.println("--------------------------------------------------------------------------------------------");
-					System.out.println("Id Trip : "+ servi.getTripId());
-					System.out.println("Id taxi : "+ servi.getTaxiId());
-					System.out.println("Total trip : "+ servi.getTripTotal());
-					System.out.println("Miles : "+ servi.getTripMiles());
-					System.out.println("Seconds : "+ servi.getTripSeconds());
-					System.out.println("--------------------------------------------------------------------------------------------");
-					
-						}
-						
-					servi= req1B.next();
-					
+
+				// Req 2A
+				IList<Servicio> listaServicios = Controller.R2A(duracion);
+				for(Servicio s : listaServicios){
+					System.out.println("IdTaxi: " + s.getTaxiId()
+							         + "  IdServicio: "+ s.getTripId()
+							         + "  Duracion: "+ s.getTripSeconds());
 				}
-				System.out.println("--------------------------------------------------------------------------------------------");
-				System.out.println("Conjunto de Serv. distancia (millas) : De "+  (((contadorDis*10)-9)==1?0:((contadorDis*10)-9)) + " a "+((contadorDis*10)) );
-				System.out.println("Numero de servicios : "+ req1B.size());
-				
-				}
-				else{
-					System.out.println("--------------------------------------------------------------------------------------------");
-					System.out.println("No hay Servicios en este rango de Distancia.");
-					System.out.println("--------------------------------------------------------------------------------------------");
-					
-					
-				}
+			
 				break;
+
+			case 4: //1B
+
+				//Distancia minima
+				double distanciaMinima = 0;
+				System.out.println("Ingrese la distancia minima (millas)");
+				String distMinReq1B = sc.next();
 				
-			case 4: //salir
+				try {
+					distanciaMinima = Double.parseDouble(distMinReq1B);
+				} catch (Exception e) {
+					System.err.println("Distancia invalida");
+					break;
+				}
+
+				//Distancia maxima
+				double distanciaMaxima = 0;
+				System.out.println("Ingrese la distancia maxima (millas)");
+				String distMaxReq1B= sc.next();
+				
+				try {
+					distanciaMaxima = Double.parseDouble(distMaxReq1B);
+				} catch (Exception e) {
+					System.err.println("Distancia invalida");
+					break;
+				}
+
+				if ( distanciaMinima > distanciaMaxima ) {
+					System.err.println("Rango de distancias invalido");
+					break;
+				}
+
+				// Req 1B
+				IList<Servicio> listaServicios2 = Controller.R1B(distanciaMinima, distanciaMaxima);
+				for(Servicio s : listaServicios2){
+					System.out.println("IdTaxi: " + s.getTaxiId()
+							         + "  IdServicio: "+ s.getTripId()
+							         + "  Distancia: "+ s.getTripMiles());
+				}
+
+				break;
+
+			case 5: //2B
+
+				//idZonaRecogida
+				System.out.println("Ingrese el id de la zona de recogida");
+				String idZonaRecogidaReq2B = sc.next();
+				int idZonaRecogida = 0;
+				try {
+					idZonaRecogida = Integer.parseInt(idZonaRecogidaReq2B);
+				}
+				catch (Exception e) {
+					System.err.println("id invalido");
+					break;
+				}
+
+				//idZonaTerminacion
+				System.out.println("Ingrese el id de la zona de terminacion");
+				String idZonaTerminacionReq2B = sc.next();
+				int idZonaTerminacion = 0;
+				try {
+					idZonaTerminacion = Integer.parseInt(idZonaTerminacionReq2B);
+				}
+				catch (Exception e) {
+					System.err.println("id invalido");
+					break;
+				}
+				
+				//Fecha Inicial
+				System.out.println("Ingrese la fecha inicial (Ej : 2017-02-01)");
+				String fechaInicialReq2B = sc.next();
+				
+				//Hora inicial
+				System.out.println("Ingrese la hora inicial (Ej: 09:00:00.000)");
+				String horaInicialReq2B = sc.next();
+				
+				//fecha final
+				System.out.println("Ingrese la fecha final (Ej : 2017-02-01)");
+				String fechaFinalReq2B = sc.next();
+				
+				//hora final
+				System.out.println("Ingrese la hora final (Ej: 09:00:00.000)");
+				String horaFinalReq2B = sc.next();
+
+				// Req 2B
+				IList<Servicio> listaServicios3 = Controller.R2B(idZonaRecogida, idZonaTerminacion, fechaInicialReq2B, fechaFinalReq2B, horaInicialReq2B, horaFinalReq2B);
+				for(Servicio s : listaServicios3){
+					System.out.println("Id Trip: "+ s.getTripId());
+					System.out.println("  Zona recogida: " + s.getPickupZone());
+					System.out.println("  Zona terminacion: " + s.getDropOffZone());
+					System.out.println("  Hora inicial: " + s.getStartTime());
+				}
+
+				break;
+
+			case 6: //1C
+				
+				TaxiConPuntos[] taxis = Controller.R1C();
+				
+				// TODO Si el conjunto ordenado tiene menos de 10 taxis, hay que mostrar todos taxis en orden ascendente de puntos. 
+				//      Por cada taxi mostrar su Id y sus puntos
+				//
+				//      Si el conjunto ordenado tiene 10 taxis o mas, hay que mostrar los 5 primeros taxis y los 5 ultimos taxis resultado del ordenamiento. 
+				//      Por cada taxi mostrar su Id y sus puntos
+				break;
+
+			case 7: //2C
+				
+				System.out.println("Ingrese el id del taxi");
+				String taxiIDReq2C = sc.next();
+
+				
+				System.out.println("Ingrese la cantidad X de millas");
+				String millasReq2C = sc.next();
+				double millas = 0;
+				try
+				{
+					millas = Double.parseDouble(millasReq2C);
+				}
+				catch(Exception e)
+				{
+					System.out.println("Cantidad invalida");
+				}
+
+				
+				System.out.println("Ingrese la latitud de referencia");
+				String latitudReq2C = sc.next();
+				double latitud = 0;
+				try
+				{
+					 latitud = Double.parseDouble(latitudReq2C);
+				}
+				catch(Exception e)
+				{
+					System.out.println("Latitud invalida");
+				}
+				
+				System.out.println("Ingrese la longitud de referencia");
+				String longitudReq2C = sc.next();
+				double longitud = 0;
+				try
+				{
+					 longitud = Double.parseDouble(longitudReq2C);
+				}
+				catch(Exception e)
+				{
+					System.out.println("Latitud invalida");
+				}
+
+				// Req 2C
+				IList<Servicio> servicios2C = Controller.R2C(taxiIDReq2C, millas, latitud, longitud);
+     			
+				for(Servicio s : servicios2C)
+				{
+					System.out.println();
+					System.out.println("Servicio: " + s.getTripId());
+					//TODO imprimir la latitud y la longitud de los servicios
+					System.out.println("  (Lat: " + s.getPickupLatitud() + ", Long: " + s.getPickupLongitud() + " )");
+					System.out.println("  Distancia (millas) a la referencia: " + "�<calcular>?");
+				}
+			
+				break;
+
+			case 8: //3C
+				
+			
+				//fecha 
+				System.out.println("Ingrese la fecha (Ej : 2017-02-01)");
+				String fecha = sc.next();
+
+				//hora 
+				System.out.println("Ingrese la hora de inicio con minutos arbitrarios en rango [0, 59] (Ej: 09:25:00.000)");
+				String hora = sc.next();
+
+				// Req 3C
+				IList<Servicio> servicios3C = Controller.R3C(fecha, hora);
+     			
+				for(Servicio s : servicios3C)
+				{
+					System.out.println();
+					System.out.println("Servicio: " + s.getTripId());
+					System.out.println("  Taxi: " + s.getTaxiId());
+					System.out.println("  Zona recogida: " + s.getPickupZone());
+					System.out.println("  Zona terminacion: " + s.getDropOffZone());
+					System.out.println("  Fecha/Hora inicial: " + s.getStartTime());
+				}
+
+				break;
+
+			case 9: 
 				fin=true;
 				sc.close();
 				break;
@@ -237,16 +367,26 @@ public class TaxiTripsManagerView
 	private static void printMenu() //
 	{
 		System.out.println("---------ISIS 1206 - Estructuras de datos----------");
-		System.out.println("---------------------TALLER 6----------------------");
-		System.out.println("Cargar data (1C):");
-		System.out.println("1. Cargar toda la informacion dada una fuente de datos (small,medium, large). Debe ingresar el rango de "+ "\n" + "fecha/hora que desea cargar.");
+		System.out.println("---------------------Proyecto 2----------------------");
+		System.out.println("Iniciar la Fuente de Datos a Consultar :");
+		System.out.println("1. Cargar toda la informacion del sistema de una fuente de datos (small, medium o large).");
 
+		System.out.println("\nParte A:\n");
+		System.out.println("2. Obtener el taxi con mas servicios en una zona dada para una determinada compania (1A)");
+		System.out.println("3. Obtener los servicios con duracion en el minuto al que corresponde una duracion de consulta (en segundos) (2A)");
 		
-		System.out.println("2. Dada un área de comunidad, retornar los servicios que se inician en esa área ordenados cronológicamente." + "\n"
-				+ " Se muestra su identificador y su tiempo de llegada. (1A) ");
-		System.out.println("3. Agrupar los servicios por la distancia recorrida en millas. (2A)");
-		System.out.println("4. Salir.");
-	
+		System.out.println("\nParte B:\n");
+		System.out.println("4. Obtener los servicios cuya distancia recorrida esta en un rango de distancia en millas (1B)");
+		System.out.println("5. Obtener los servicios que iniciaron en una zona de recogida y terminaron en una zona de terminacion entre una fecha/hora inicial y una fecha/Hora final (2B)");
+		
+
+		System.out.println("\nParte C:\n");
+		System.out.println("6. Ordenamiento de taxis utilizando un sistema de puntos (1C)");
+		System.out.println("7. Informar las localizaciones geograficas (latitud,longitud) de los servicios iniciados por un taxi que estan dentro de la zona a X millas de una localizacion geografica (latitud, longitud) dada (2C)");
+		System.out.println("8. Obtener los servicios que inician en el rango de 15 minutos mas cercano a una fecha/hora dadas (con minutos arbitrarios en rango [0, 59]), saliendo de una zona y terminado en otra zona (3C)");
+		System.out.println("9. Salir");
+		System.out.println("Ingrese el numero de la opcion seleccionada y presione <Enter> para confirmar: (e.g., 1):");
+
 	}
 
 	private static void printMenuCargar()
@@ -255,7 +395,7 @@ public class TaxiTripsManagerView
 		System.out.println("-- 1. Small");
 		System.out.println("-- 2. Medium");
 		System.out.println("-- 3. Large");
-		System.out.println("-- Type the option number for the task, then press enter: (e.g., 1)");
+		System.out.println("-- Ingrese el numero de la fuente a cargar y presione <Enter> para confirmar: (e.g., 1)");
 	}
 
 }
